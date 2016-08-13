ef72021a628661bf746183d7948e2415076132a2
Laksh Arora <techedlaksh@users.noreply.github.com>
2016-08-13 13:06:44 +0530

    main screen added

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+    
+    public void LoadLevel(string name)
+    {
+        //Debug.Log("Level loaded " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void StopLevel(string name)
+    {
+        Debug.Log("Level Stopped " + name);
+    }
+}
